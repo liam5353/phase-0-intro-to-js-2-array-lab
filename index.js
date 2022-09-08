3eb1207d0ff3ff32a0e12ceb2a8f8ec6f66022be
“Liam <okeefe.liam@yahoo.com>
2022-09-07 23:36:03 -0500

    commit tree 4262b521e8fd419ac6974fff54e5d4ba07670b67

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const allCats = [...cats]
+    allCats.push(name)
+    return allCats
+}
+
+function prependCat(name) {
+    const allCats = [...cats]
+    allCats.unshift(name)
+    return allCats
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1)
+ }
+
+function removeFirstCat() {
+    return cats.slice(cats.length - 2);
+ }
